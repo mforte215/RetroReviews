6fb52961eb1eefae6b6bd40f667cdc926b122469
MForte101 <mark.fortee@gmail.com>
2020-04-30 08:56:58 -0400

    reconfigured urls and added movie feed

--- application/urls.py
+++ application/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.newIndexView, name='index'),
     path('<int:article_id>/', views.newDetailView, name='detail'),
     path('video/<int:video_id>/', views.videoDetail, name='detailvideo'),
+    path('movies/', views.movieFeed, name='moviefeed')
 ]
